30e7214d6d9e1c3625c83a5f533a174f33955ed3
devzeemalik <devzeeshanmalik@gmail.com>
2023-05-25 11:23:51 +0500

    fix-emergency-issue

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-echo 'this is frist commit';
+echo 'this is frist commit & Hell is empty and devils are here';
 
-echo 'This is second commit';
+echo 'This is second commit, once upon';
 
-echo 'This is third commit';
+echo 'This is third commit, a time';
 
-echo 'This is fourth commit';
+echo 'This is fourth commit, there ';
 echo 'This is fifth commit';
